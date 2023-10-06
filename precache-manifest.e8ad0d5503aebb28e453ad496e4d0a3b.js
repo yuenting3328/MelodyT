fc7e8deea11ebeb7fb8d7ac54d75d24af5823653
yuenting3328 <63846462+yuenting3328@users.noreply.github.com>
2023-10-06 09:13:17 +0800

    Updates

---
+++ precache-manifest.e8ad0d5503aebb28e453ad496e4d0a3b.js
@@ -0,0 +1,430 @@
+self.__precacheManifest = [
+  {
+    "revision": "29dfce9071027b6f2a5d",
+    "url": "/yuenting3328/melodyt/static/css/main.1da344be.chunk.css"
+  },
+  {
+    "revision": "29dfce9071027b6f2a5d",
+    "url": "/yuenting3328/melodyt/static/js/main.29dfce90.chunk.js"
+  },
+  {
+    "revision": "a6f3b45451dbda57ea01",
+    "url": "/yuenting3328/melodyt/static/js/1.a6f3b454.chunk.js"
+  },
+  {
+    "revision": "872123af2210f2120000",
+    "url": "/yuenting3328/melodyt/static/js/runtime~main.872123af.js"
+  },
+  {
+    "revision": "08d59825aa09f741c9272b85e6a1adbf",
+    "url": "/yuenting3328/melodyt/static/media/PlusJakartaSans-SemiBold.08d59825.ttf"
+  },
+  {
+    "revision": "9fb6d71fc9edf9db455adeb04da9795c",
+    "url": "/yuenting3328/melodyt/static/media/PlusJakartaSans-Medium.9fb6d71f.ttf"
+  },
+  {
+    "revision": "f539ae4a8884822210a4553cc88ca3dc",
+    "url": "/yuenting3328/melodyt/static/media/PlusJakartaSans-Regular.f539ae4a.ttf"
+  },
+  {
+    "revision": "d6fb190614a9297c6263f85094463592",
+    "url": "/yuenting3328/melodyt/static/media/PlusJakartaSans-Light.d6fb1906.ttf"
+  },
+  {
+    "revision": "9bfdb6bb1fda2806463b64e44c5eaed9",
+    "url": "/yuenting3328/melodyt/static/media/PlusJakartaSans-ExtraBold.9bfdb6bb.ttf"
+  },
+  {
+    "revision": "fa53254a5fc2921f7a731b5fb78423bd",
+    "url": "/yuenting3328/melodyt/static/media/icon photo.fa53254a.png"
+  },
+  {
+    "revision": "470985ffbf492fc5c388f8ad62d41e5c",
+    "url": "/yuenting3328/melodyt/static/media/sfc1.470985ff.gif"
+  },
+  {
+    "revision": "7282ae9bcb1e899e764d603089a8f76c",
+    "url": "/yuenting3328/melodyt/static/media/muji.7282ae9b.gif"
+  },
+  {
+    "revision": "afd9db9a2bdf252376fd05235ae7f399",
+    "url": "/yuenting3328/melodyt/static/media/empf.afd9db9a.gif"
+  },
+  {
+    "revision": "c06fc6d567b1c208378df66709abea10",
+    "url": "/yuenting3328/melodyt/static/media/motic1.c06fc6d5.png"
+  },
+  {
+    "revision": "60df4b9f282a19e505305e8b97d96643",
+    "url": "/yuenting3328/melodyt/static/media/ha1.60df4b9f.gif"
+  },
+  {
+    "revision": "ed32599acd58f4aca31d3096c83a1783",
+    "url": "/yuenting3328/melodyt/static/media/about icon.ed32599a.png"
+  },
+  {
+    "revision": "b32a8cc6f531e184c5ff26f38435c9f1",
+    "url": "/yuenting3328/melodyt/static/media/about icon2.b32a8cc6.png"
+  },
+  {
+    "revision": "6af3b3220f59b5b0cfc7e503222f2594",
+    "url": "/yuenting3328/melodyt/static/media/pccw solutions.6af3b322.png"
+  },
+  {
+    "revision": "3692500a1ed0718e9345a5abbd707748",
+    "url": "/yuenting3328/melodyt/static/media/sfc logo.3692500a.png"
+  },
+  {
+    "revision": "dac5f847ff1dff6ce690f78f016cd6fa",
+    "url": "/yuenting3328/melodyt/static/media/asl logo.dac5f847.png"
+  },
+  {
+    "revision": "41de40ddd82c43a55aeccf88561d7fcc",
+    "url": "/yuenting3328/melodyt/static/media/mpfa logo.41de40dd.png"
+  },
+  {
+    "revision": "3fdeab588944db673bb2c6d6262ddea4",
+    "url": "/yuenting3328/melodyt/static/media/Melody Tse_CV_2023.3fdeab58.pdf"
+  },
+  {
+    "revision": "b926e348f6cfb5051a516d32f3c0d490",
+    "url": "/yuenting3328/melodyt/static/media/Adobe_Illustrator_CC_icon 7.b926e348.png"
+  },
+  {
+    "revision": "36710269cd173108a3a3077eed7067db",
+    "url": "/yuenting3328/melodyt/static/media/Adobe_Illustrator_CC_icon 8.36710269.png"
+  },
+  {
+    "revision": "afc5bb5d2841ce1cccaa471a502bf6b8",
+    "url": "/yuenting3328/melodyt/static/media/Adobe_Illustrator_CC_icon 10.afc5bb5d.png"
+  },
+  {
+    "revision": "ccf8b575ac45348eac0c4ad442028622",
+    "url": "/yuenting3328/melodyt/static/media/Adobe_Illustrator_CC_icon 14.ccf8b575.png"
+  },
+  {
+    "revision": "1df36121e9d11512d3e5cd7b922ebdb1",
+    "url": "/yuenting3328/melodyt/static/media/peter.1df36121.png"
+  },
+  {
+    "revision": "3e7784286fae1d10c874627c23b98a0b",
+    "url": "/yuenting3328/melodyt/static/media/zoe.3e778428.png"
+  },
+  {
+    "revision": "114eb9436cfb7c3285e645c1b1b4b12e",
+    "url": "/yuenting3328/melodyt/static/media/ashely.114eb943.png"
+  },
+  {
+    "revision": "c7638173bd881b59e4a790d766268939",
+    "url": "/yuenting3328/melodyt/static/media/micky.c7638173.png"
+  },
+  {
+    "revision": "559d805b48666df836ca53952d4f9dc2",
+    "url": "/yuenting3328/melodyt/static/media/Reference-Tse Melody-201230.docx.559d805b.pdf"
+  },
+  {
+    "revision": "cb848826a253c1a0a1a27421543ad6f6",
+    "url": "/yuenting3328/melodyt/static/media/Reference-Tse Melody-20230620.cb848826.pdf"
+  },
+  {
+    "revision": "9d9e4b9eacd5fc5b871a309881868711",
+    "url": "/yuenting3328/melodyt/static/media/Bg.9d9e4b9e.png"
+  },
+  {
+    "revision": "4d59736ae18924e80e27c7a7feb6366d",
+    "url": "/yuenting3328/melodyt/static/media/HA goal.4d59736a.png"
+  },
+  {
+    "revision": "2b6c74f58b70ddf680896b26591d63aa",
+    "url": "/yuenting3328/melodyt/static/media/HA problem.2b6c74f5.png"
+  },
+  {
+    "revision": "1f056012259fcefc77f40ebeb544b14e",
+    "url": "/yuenting3328/melodyt/static/media/HA research.1f056012.png"
+  },
+  {
+    "revision": "8e4c8bfb42f04fa4ddb3f0a4b734e831",
+    "url": "/yuenting3328/melodyt/static/media/HA outcome.8e4c8bfb.png"
+  },
+  {
+    "revision": "c159f099c6b90ddf7de49e2c493a0c6f",
+    "url": "/yuenting3328/melodyt/static/media/muji backgound.c159f099.png"
+  },
+  {
+    "revision": "fc9ba9509e69e804f282723838772154",
+    "url": "/yuenting3328/melodyt/static/media/muji outcome1.fc9ba950.png"
+  },
+  {
+    "revision": "8aee8d93e444d6dc6505df542efbf181",
+    "url": "/yuenting3328/melodyt/static/media/muji outcome2.8aee8d93.png"
+  },
+  {
+    "revision": "e328b7a92e738283972d86974cb16a40",
+    "url": "/yuenting3328/melodyt/static/media/muji outcome3.e328b7a9.png"
+  },
+  {
+    "revision": "7995b4ec4c2c3e076cee46f52eff9f1d",
+    "url": "/yuenting3328/melodyt/static/media/muji outcome4.7995b4ec.png"
+  },
+  {
+    "revision": "eb10635aac8b01a3a475fd57648eb33c",
+    "url": "/yuenting3328/melodyt/static/media/muji product design.eb10635a.png"
+  },
+  {
+    "revision": "91f6f63a05d9c32d506b8e1aeddae73a",
+    "url": "/yuenting3328/melodyt/static/media/muji ui1.91f6f63a.png"
+  },
+  {
+    "revision": "5e519ad7fb16dd147a54d2247bbef158",
+    "url": "/yuenting3328/melodyt/static/media/muji ui2.5e519ad7.png"
+  },
+  {
+    "revision": "b0f301ad320268cc22ab009103422d20",
+    "url": "/yuenting3328/melodyt/static/media/muji ui3.b0f301ad.png"
+  },
+  {
+    "revision": "747be06c4c8fa67452573fc7dc38b0e2",
+    "url": "/yuenting3328/melodyt/static/media/muji ui4.747be06c.png"
+  },
+  {
+    "revision": "6352e029ded9a367b3dea654580bdde1",
+    "url": "/yuenting3328/melodyt/static/media/muji ui5.6352e029.png"
+  },
+  {
+    "revision": "3fb470a68cac3d5c3a686ff2b1442604",
+    "url": "/yuenting3328/melodyt/static/media/muji prototype.3fb470a6.gif"
+  },
+  {
+    "revision": "e90f50aaac29ea724afcc3f6b5d32972",
+    "url": "/yuenting3328/melodyt/static/media/wings goal.e90f50aa.png"
+  },
+  {
+    "revision": "68d182c12f41bd67264bcf225706a969",
+    "url": "/yuenting3328/melodyt/static/media/wings design.68d182c1.png"
+  },
+  {
+    "revision": "5eb12c01200f403f0c575f161fd48584",
+    "url": "/yuenting3328/melodyt/static/media/wings wireframe.5eb12c01.png"
+  },
+  {
+    "revision": "efa8998d6b1690496df3efb71f9c2c37",
+    "url": "/yuenting3328/melodyt/static/media/wings video.efa8998d.png"
+  },
+  {
+    "revision": "bf5425dfc203b54778d713ed33e85f3f",
+    "url": "/yuenting3328/melodyt/static/media/wings eIP.bf5425df.png"
+  },
+  {
+    "revision": "88f907acf894bc1044ce32cab7f3748c",
+    "url": "/yuenting3328/melodyt/static/media/wings research.88f907ac.png"
+  },
+  {
+    "revision": "6aac817bd05740f96e780a4a3c01cdab",
+    "url": "/yuenting3328/melodyt/static/media/wings research2.6aac817b.png"
+  },
+  {
+    "revision": "35275ca009d68ba83dbc1eac046640c2",
+    "url": "/yuenting3328/melodyt/static/media/wings persona.35275ca0.png"
+  },
+  {
+    "revision": "3d93ad5f26e20a11b0f360b93af06a5e",
+    "url": "/yuenting3328/melodyt/static/media/wings storyboard.3d93ad5f.png"
+  },
+  {
+    "revision": "7c5e7c40e8d73894da94957895a387f0",
+    "url": "/yuenting3328/melodyt/static/media/wings eIP wireframe.7c5e7c40.png"
+  },
+  {
+    "revision": "99c657ff8293a91c53ca7a9fd415bc22",
+    "url": "/yuenting3328/melodyt/static/media/wings design system.99c657ff.png"
+  },
+  {
+    "revision": "c05f52f856b28ed987e9d39c73f3c6a1",
+    "url": "/yuenting3328/melodyt/static/media/sfc prototype.c05f52f8.gif"
+  },
+  {
+    "revision": "618855fb10d6448d2beba6d7b7998c97",
+    "url": "/yuenting3328/melodyt/static/media/empf goal.618855fb.png"
+  },
+  {
+    "revision": "9e1419fe213bf854ead17088489b7ecd",
+    "url": "/yuenting3328/melodyt/static/media/empf design.9e1419fe.png"
+  },
+  {
+    "revision": "172368c359e654cb452d71097d99acd4",
+    "url": "/yuenting3328/melodyt/static/media/empf wireframe.172368c3.gif"
+  },
+  {
+    "revision": "fc01da5f0a5293b9eb6538d251b7f8c3",
+    "url": "/yuenting3328/melodyt/static/media/empf styleguide.fc01da5f.png"
+  },
+  {
+    "revision": "d3428d65147cf1ae08689e7ae1fcc60f",
+    "url": "/yuenting3328/melodyt/static/media/empf development.d3428d65.gif"
+  },
+  {
+    "revision": "4c0bd0b512cbce3fc9b7987c5f3ede10",
+    "url": "/yuenting3328/melodyt/static/media/empf prototype.4c0bd0b5.gif"
+  },
+  {
+    "revision": "002a28901e716efb8ce597d854a50a79",
+    "url": "/yuenting3328/melodyt/static/media/motic challenge1.002a2890.png"
+  },
+  {
+    "revision": "3f86bca92799459a7bb633c7746df53c",
+    "url": "/yuenting3328/melodyt/static/media/motic challenge2.3f86bca9.png"
+  },
+  {
+    "revision": "23530c5a9ecf736d12a6a677c8b2804f",
+    "url": "/yuenting3328/melodyt/static/media/motic solution1.23530c5a.png"
+  },
+  {
+    "revision": "b45cd765e09a3e7c18e29d53e79e1baf",
+    "url": "/yuenting3328/melodyt/static/media/motic solution2.b45cd765.png"
+  },
+  {
+    "revision": "c456148ef6f8b22b17bfaa1489ac0b35",
+    "url": "/yuenting3328/melodyt/static/media/motic  design.c456148e.png"
+  },
+  {
+    "revision": "1564ae3a513d6deacd506122dce9d03f",
+    "url": "/yuenting3328/melodyt/static/media/motic wireframe1.1564ae3a.png"
+  },
+  {
+    "revision": "06f5a4c1abbd57f35abde2d5551510e8",
+    "url": "/yuenting3328/melodyt/static/media/motic wireframe2.06f5a4c1.png"
+  },
+  {
+    "revision": "4c72465942282bc50f4a7023f797a0df",
+    "url": "/yuenting3328/melodyt/static/media/motic outcome1.4c724659.png"
+  },
+  {
+    "revision": "b432ad1210897764a07d77eff02a818b",
+    "url": "/yuenting3328/melodyt/static/media/motic outcome2.b432ad12.png"
+  },
+  {
+    "revision": "ec6572bb9a6d69cbe4ec4ef1e9e47379",
+    "url": "/yuenting3328/melodyt/static/media/seal concept.ec6572bb.png"
+  },
+  {
+    "revision": "ee1457014b7adad45ffeba1623e7b69f",
+    "url": "/yuenting3328/melodyt/static/media/seal work.ee145701.png"
+  },
+  {
+    "revision": "b199e63103449b1e05dfffa90fa58c93",
+    "url": "/yuenting3328/melodyt/static/media/seal work1.b199e631.png"
+  },
+  {
+    "revision": "21b485f94bf808246a8fa8a2206b6e84",
+    "url": "/yuenting3328/melodyt/static/media/seal work2.21b485f9.png"
+  },
+  {
+    "revision": "dcedae8b43396b732aec7021ef3eb0f3",
+    "url": "/yuenting3328/melodyt/static/media/seal work3.dcedae8b.png"
+  },
+  {
+    "revision": "2a04e858c614f597e8bb5d0555122dff",
+    "url": "/yuenting3328/melodyt/static/media/seal work4.2a04e858.png"
+  },
+  {
+    "revision": "99ac2906027a5377df90dcc1c20558a5",
+    "url": "/yuenting3328/melodyt/static/media/seal work5.99ac2906.png"
+  },
+  {
+    "revision": "7a2ac4251ab7b20a7d6647692a04b88f",
+    "url": "/yuenting3328/melodyt/static/media/experience concept.7a2ac425.png"
+  },
+  {
+    "revision": "54b7e778b6f4e6815fb604613f5aa174",
+    "url": "/yuenting3328/melodyt/static/media/experience design.54b7e778.png"
+  },
+  {
+    "revision": "94c01fc8e1fc0c68b130c0192c6036f4",
+    "url": "/yuenting3328/melodyt/static/media/research photos.94c01fc8.png"
+  },
+  {
+    "revision": "6e8c4998d4c0d15460781a4d9f6184d5",
+    "url": "/yuenting3328/melodyt/static/media/installation concept.6e8c4998.png"
+  },
+  {
+    "revision": "e0a2ed3a6bfcefa75ffe02440b2d0283",
+    "url": "/yuenting3328/melodyt/static/media/installation outcome.e0a2ed3a.png"
+  },
+  {
+    "revision": "1ea89531be171402716d999718ef044b",
+    "url": "/yuenting3328/melodyt/static/media/installation outcome side 1.1ea89531.png"
+  },
+  {
+    "revision": "791a48218dc0be9de62fbcd1a94b74f5",
+    "url": "/yuenting3328/melodyt/static/media/installation outcome side 2.791a4821.png"
+  },
+  {
+    "revision": "7dae244963714ee2b16fcbab46661792",
+    "url": "/yuenting3328/melodyt/static/media/PlusJakartaSans-Bold.7dae2449.ttf"
+  },
+  {
+    "revision": "5bd0aa8ba9e078f3802d6057edf85095",
+    "url": "/yuenting3328/melodyt/static/media/ha banner ipad.5bd0aa8b.png"
+  },
+  {
+    "revision": "103b472c1c8853a91c293cb62add023d",
+    "url": "/yuenting3328/melodyt/static/media/HA banner.103b472c.png"
+  },
+  {
+    "revision": "f3484868d357b2b6a37ff6a5f9b863aa",
+    "url": "/yuenting3328/melodyt/static/media/muji banner.f3484868.png"
+  },
+  {
+    "revision": "e4df200cb246f194154801b077828a02",
+    "url": "/yuenting3328/melodyt/static/media/muji banner ipad.e4df200c.png"
+  },
+  {
+    "revision": "a1ce902b51dc660163c677f93524c3b6",
+    "url": "/yuenting3328/melodyt/static/media/wings banner ipad.a1ce902b.png"
+  },
+  {
+    "revision": "9ef50d6deaa1deea779dc7e18de85b69",
+    "url": "/yuenting3328/melodyt/static/media/wings banner.9ef50d6d.png"
+  },
+  {
+    "revision": "7d69f592b1f47f2c73d286bfee579822",
+    "url": "/yuenting3328/melodyt/static/media/empf banner ipad.7d69f592.png"
+  },
+  {
+    "revision": "49f5be851090e942d503f92e8ff58eb4",
+    "url": "/yuenting3328/melodyt/static/media/empf banner.49f5be85.png"
+  },
+  {
+    "revision": "0910ce515dcc5fc673ce7e4ed1dc8ea8",
+    "url": "/yuenting3328/melodyt/static/media/motic banner ipad.0910ce51.png"
+  },
+  {
+    "revision": "23fbfb9660c2d00a52202ee8ff2b06d7",
+    "url": "/yuenting3328/melodyt/static/media/motic banner.23fbfb96.png"
+  },
+  {
+    "revision": "8b884bea28b9046f760a5f16420ccf61",
+    "url": "/yuenting3328/melodyt/static/media/seal banner ipad.8b884bea.png"
+  },
+  {
+    "revision": "6b7105cd1622bcefefaf5a9074b67a3d",
+    "url": "/yuenting3328/melodyt/static/media/seal banner.6b7105cd.png"
+  },
+  {
+    "revision": "153cd550c4ce71cb0f8ca6ea754498d4",
+    "url": "/yuenting3328/melodyt/static/media/experience banner ipad.153cd550.png"
+  },
+  {
+    "revision": "ca667e64eedf67d351cbeb65896f01ef",
+    "url": "/yuenting3328/melodyt/static/media/experience banner.ca667e64.png"
+  },
+  {
+    "revision": "8c50960995c0b0c2b7838b8f12ad258a",
+    "url": "/yuenting3328/melodyt/static/media/installation banner.8c509609.png"
+  },
+  {
+    "revision": "2463b27b0552fa0e488a0e6d792b3116",
+    "url": "/yuenting3328/melodyt/index.html"
+  }
+];
