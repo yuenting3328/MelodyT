0bab7ce9d114f9b2b489c27ec7f9ae0f97385759
yuenting3328 <63846462+yuenting3328@users.noreply.github.com>
2023-10-11 11:48:16 +0800

    find company logo lost in 480px > div.about should > 900px

--- src/assets/components/SelfDescription/selfDescription.js
+++ src/assets/components/SelfDescription/selfDescription.js
@@ -13,45 +13,45 @@ import cvPdf from "../../pdf/Melody Tse_CV_2023.pdf";
 const SelfDescription =() => {
  return(
     <div className="about">
-<div className="frame-wrapper">
-<div className="frame-2">
-<div className="intro">
-<img className="icon" alt="Icon" src={bg2} />
-<div className="resume">
-<p className="i-m-melody-tse-a-UX">
-I&#39;m Melody Tse, a UX/UI Designer who is passionate about using technology and design to enhance the
-lives of others. My expertise lies in taking ideas from wireframe to prototype and conducting user
-research, interaction design, and practical operations for several corporates. <br />
-<br />
-With over 4 years of hands-on experience in digital product design, I have devoted the past 3 years to
-specialize in User Experience and User Interface. Born and raised in Hong Kong, I have worked for
-companies in over 7 industry sectors, creating digital applications and websites. <br />
-<br />
-Currently, I am focused on designing system design and implementation services for investment product
-electronic applications on WINGS, a common platform for world-renowned SFC electronic forms and
-submission services.
-</p>
-<div className="aboutBtn">
-<a href = {cvPdf} ><ResumeBtn /></a>
-</div>
-</div>
-</div>
-<div className="icon-list">
-<div className="experience">
-<img className="vector" alt="Vector" src={line} />
-<div className="text-wrapper">Professional Experience:</div>
-</div>
-<div className="icons">
-<img className="logo-en" alt="Logo en" src={pccw} />
-<img className="securities-and" alt="Securities and" src={sfc} />
-<img className="logo-mpfa" alt="Logo mpfa" src={mpfa} />
-<img className="asl-logo-combo" alt="Asl logo combo" src={asl} />
-<img className="motic" alt="motic" src={motic} />
-</div>
-</div>
-</div>
-</div>
-</div>            
+        <div className="frame-wrapper">
+            <div className="frame-2">
+                <div className="intro">
+                    <img className="icon" alt="Icon" src={bg2} />
+                    <div className="resume">
+                        <p className="i-m-melody-tse-a-UX">
+                        I&#39;m Melody Tse, a UX/UI Designer who is passionate about using technology and design to enhance the
+                        lives of others. My expertise lies in taking ideas from wireframe to prototype and conducting user
+                        research, interaction design, and practical operations for several corporates. <br />
+                        <br />
+                        With over 4 years of hands-on experience in digital product design, I have devoted the past 3 years to
+                        specialize in User Experience and User Interface. Born and raised in Hong Kong, I have worked for
+                        companies in over 7 industry sectors, creating digital applications and websites. <br />
+                        <br />
+                        Currently, I am focused on designing system design and implementation services for investment product
+                        electronic applications on WINGS, a common platform for world-renowned SFC electronic forms and
+                        submission services.
+                        </p>
+                        <div className="aboutBtn">
+                        <a href = {cvPdf} ><ResumeBtn /></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="icon-list">
+                    <div className="experience">
+                    <img className="vector" alt="Vector" src={line} />
+                        <div className="text-wrapper">Professional Experience:</div>
+                    </div>
+                    <div className="icons">
+                    <img className="logo-en" alt="Logo en" src={pccw} />
+                    <img className="securities-and" alt="Securities and" src={sfc} />
+                    <img className="logo-mpfa" alt="Logo mpfa" src={mpfa} />
+                    <img className="asl-logo-combo" alt="Asl logo combo" src={asl} />
+                    <img className="motic" alt="motic" src={motic} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>            
  )
 }
 export default SelfDescription
